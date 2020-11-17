4661e3a287cbd84d88c6b5453fcb026bcb3b992f
Matthew <matthew.runswick@gmail.com>
2020-11-17 20:51:52 +0000

    final code, all graphs formatted and labeled correctly

--- assignment6.py
+++ assignment6.py
@@ -17,11 +17,11 @@ dummy_Y = [0, 1, 0]
 
 #Part (i)
 #Part A
-
 X_range = np.linspace(-3,3, num=100)
 X_range = X_range.reshape(-1,1)
 
 distance_parameters = [0, 1, 5, 10, 25]
+dummy_predictions_kNeighbours = []
 for d_param in distance_parameters:
 
     def gaussian_kernel(distances):
@@ -30,36 +30,47 @@ for d_param in distance_parameters:
 
     dummy_model = KNeighborsRegressor(n_neighbors=3,weights=gaussian_kernel).fit(dummy_X, dummy_Y)
     ypred = dummy_model.predict(X_range)
+    dummy_predictions_kNeighbours.append(ypred)
 
-    plt.figure()
-    plt.title("γ={} - predictions".format(d_param))
-    plt.xlabel("input x"); plt.ylabel("predicted output y")
-    plt.scatter(dummy_X, dummy_Y, color='red', marker='+')
-    plt.plot(X_range, ypred, color='green', )
-    plt.show()
+plt.figure()
+plt.title("KNeighborsRegressor Predictions Over Dummy Data")
+plt.xlabel("input x"); plt.ylabel("predicted output y")
+plt.scatter(dummy_X, dummy_Y, color='red', marker='+', s=200, label="datapoint")
+col_and_label = [["orange", "γ=0"], ["green", "γ=1"], ["black", "γ=5"], ["blue", "γ=10"], ["purple", "γ=25"]]
+i = 0
+for d_param in distance_parameters:
+    plt.plot(X_range, dummy_predictions_kNeighbours[i], color=col_and_label[i][0], label=col_and_label[i][1])
+    i = i + 1
+plt.legend()
+plt.show()
 
-#Part C
+# Part C
 C_values = [0.1, 1, 10, 100, 1000]
 distance_parameters = [0, 1, 5, 10, 25]
 
 for d_param in distance_parameters:
+    dummy_predictions_KernelRidge = []
     for C in C_values:
         new_model = KernelRidge(alpha=1.0/C, kernel='rbf', gamma=d_param).fit(dummy_X, dummy_Y)
         ypred_new = new_model.predict(X_range)
-        plt.figure()
-        plt.plot(X_range, ypred_new, color='green')
-        plt.xlabel("input x"); plt.ylabel("predicted output y")
-        plt.scatter(dummy_X, dummy_Y, color='red', marker='+')
-        plt.title("γ={} C={} - predictions".format(d_param, C))
-
+        dummy_predictions_KernelRidge.append(ypred_new)
         print("γ=", d_param, "C=", C, new_model.dual_coef_)
+    plt.figure()
+    plt.plot(X_range, ypred_new, color='green')
+    plt.xlabel("input x"); plt.ylabel("predicted output y")
+    plt.scatter(dummy_X, dummy_Y, color='red', marker='+', s=200, label="datapoint")
+    plt.title("KernelRidge Predictions for Dummy Data for various C values γ={}".format(d_param))
+    col_and_label = [["orange", "C=0.1"], ["green", "c=1"], ["black", "c=10"], ["blue", "c=100"], ["purple", "c=1000"]]
+    i = 0
+    for d_param in distance_parameters:
+        plt.plot(X_range, dummy_predictions_KernelRidge[i], color=col_and_label[i][0], label=col_and_label[i][1])
+        i = i + 1
+    plt.legend()
     plt.show()
 
-#part (ii)
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2) #*************(potentially need to use, talk to others)*****************
-
+# part (ii)
 #Part A
-distance_parameters = [0, 1, 5, 10, 25, 50]
+distance_parameters = [0, 1, 5, 10, 25]
 kf = KFold(n_splits = 5)
 mean_values_KNeighbors = []
 standard_deviation_values_KNeighbors =[]
@@ -86,8 +97,8 @@ for d_param in distance_parameters:
 plt.figure()
 plt.title("predictions over real data for various γ values")
 plt.xlabel("input x"); plt.ylabel("predicted output y")
-plt.scatter(X, y, color='red', marker='+')
-col_and_label = [["red", "γ=0"], ["green", "γ=1"], ["black", "γ=5"], ["blue", "γ=10"], ["yellow", "γ=25"], ["cyan", "γ=50"]]
+plt.scatter(X, y, color='red', marker='+', label="datapoint")
+col_and_label = [["orange", "γ=0"], ["green", "γ=1"], ["black", "γ=5"], ["blue", "γ=10"], ["purple", "γ=25"]]
 i = 0
 for ypred in predictions_KNeighbors:
     plt.plot(X_range, ypred, color=col_and_label[i][0], label=col_and_label[i][1] )
@@ -95,8 +106,6 @@ for ypred in predictions_KNeighbors:
 plt.legend()
 plt.show()
 
-print("mean values", mean_values_KNeighbors)
-print("standard deviation values", standard_deviation_values_KNeighbors)
 fig = plt.figure()
 plt.title("γ Value vs Average Mean Squared Errors With Standard Deviation")
 plt.errorbar(distance_parameters, mean_values_KNeighbors, yerr=standard_deviation_values_KNeighbors)
@@ -131,8 +140,8 @@ for d_param in distance_parameters:
     plt.figure()
     plt.title("predictions over real data for various C values for γ={}".format(d_param))
     plt.xlabel("input x"); plt.ylabel("predicted output y")
-    plt.scatter(X, y, color='red', marker='+')
-    col_and_label = [["red", "C=0.1"], ["green", "C=1"], ["black", "C=10"], ["blue", "C=100"], ["yellow", "C=1000"]]
+    plt.scatter(X, y, color='red', marker='+', label="datapoint")
+    col_and_label = [["orange", "C=0.1"], ["green", "C=1"], ["black", "C=10"], ["blue", "C=100"], ["purple", "C=1000"]]
     i = 0
     for ypred in predictions_KernelRidge:
         plt.plot(X_range, ypred, color=col_and_label[i][0], label=col_and_label[i][1] )
@@ -140,16 +149,8 @@ for d_param in distance_parameters:
     plt.legend()
 
     fig = plt.figure()
-    plt.title("C Value vs Average Mean Squared Errors With Standard Deviation")
+    plt.title("C Value vs Average Mean Squared Errors With Standard Deviation γ={}".format(d_param))
     plt.errorbar(C_values, mean_values_KernelRidge, yerr=standard_deviation_values_KernelRidge)
     plt.xscale("log")
-    plt.xlabel("gamma Values");plt.ylabel("Average of Mean Squared Errors")
+    plt.xlabel("C Values");plt.ylabel("Average of Mean Squared Errors")
     plt.show()
-
-
-
-#questions for dave
-#do u split data 20/80 test/training
-#calculating error over real values not over full X_range (-3, 3)
-#do u check every gamma value for every C value 5 times (a lot of graphs)
-#do u do kfold for both the C values and the gamma values?
